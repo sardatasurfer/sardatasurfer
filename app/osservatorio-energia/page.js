d3322a7e601669592c53b7a991df401fd873eb92
SarDataSurfer <admin@sardatasurfer.it>
2025-11-11 10:39:09 +0100

    Create page.js

---
+++ app/osservatorio-energia/page.js
@@ -0,0 +1,8 @@
+export default function Energia() {
+  return (
+    <>
+      <h1>Osservatorio Energia Sardegna</h1>
+      <p>Qui compariranno grafici aggiornati su produzione per fonte, quote FER e trend.</p>
+    </>
+  );
+}
