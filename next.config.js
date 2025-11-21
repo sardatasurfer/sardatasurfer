64c7d7fa7d51f59bc2511f1fcd7fdadc7ddae79a
SarDataSurfer <admin@sardatasurfer.it>
2025-11-21 13:07:58 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')();
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx'],  // Abilita MDX
-  reactStrictMode: true,  // Mantieni la tua configurazione
+  pageExtensions: ['js', 'jsx', 'mdx'],
   experimental: {
-    mdxRs: true,  // Fixa conflitti React e prerendering
+    mdxRs: true,
   },
 });
