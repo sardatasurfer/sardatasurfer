c4ff8d57b37cedf0315186d7cb1bfbeeba4986b3
SarDataSurfer <admin@sardatasurfer.it>
2025-11-24 15:35:06 +0100

    Update PostClientWrapper.js

--- app/blog/PostClientWrapper.js
+++ app/blog/PostClientWrapper.js
@@ -3,7 +3,7 @@
 import { use } from 'react';
 
 export default function PostClientWrapper({ slug, MDXComponents }) {
-  const PostModule = use(import(`@/posts/${slug}.mdx`));
+  const PostModule = use(import(`./posts/${slug}.mdx`));
   const Post = PostModule.default;
 
   return <Post components={MDXComponents} />;
